605fe6f63b92ae3fabe5de71b55217ef767a9612
Nbaut63221 <nbaut63221@my.sunywcc.edu>
2025-05-20 19:44:57 -0400

    Add files via upload

---
+++ talkback.py
@@ -0,0 +1,26 @@
+# I want this programt to ask the user
+# their name "what's your name?" and then
+# I want the program to "HI, NAME"
+# back where NAME is the user's actual name
+# ask the user what year were they born?
+# ask the user whats their favorite color?
+#back where COLOR is the user's color
+#back where YEAR is the user's year of birth
+
+
+# step 1, ask the user what their name is
+print(" what's your name?")
+# step 2: input the user's name front the keyboard
+# and save it into a variable so we can save it later
+user_name = input()
+# step 3: say hi back using the user's name
+print(" Hi,"+ user_name + ".")
+
+
+print("what year were you born?")
+year_born = input()
+print("very nice,"+ user_name + ".")
+
+print("what's your favorite color?")
+favorite_color = input ()
+print("that's a great color!" + ".")
